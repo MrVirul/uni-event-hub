e27bbc0a0e483fc845df1ccaff7f36f8280ed1ca
MrVirul <virul.mmeemana@gmail.com>
2026-04-19 22:14:14 +0530

    feat: add user model to backend

---
+++ backend/src/Models/User.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default userSchema;
