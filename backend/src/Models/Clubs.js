b1a4991a497371dcf001e388901e20ce3326767c
MrVirul <virul.mmeemana@gmail.com>
2026-05-01 12:48:14 +0530

    Add Clubs model, update upload middleware, and clean up frontend files

---
+++ backend/src/Models/Clubs.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const clubsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    
+})
